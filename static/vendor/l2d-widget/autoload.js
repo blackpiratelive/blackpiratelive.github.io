061caaf814789dca52c2c0374da2e154dd1ecc08
blackpirateapps <github@blackpiratex.com>
2026-07-21 02:06:14 +0530

    added interactive Live2D kanban-musume widget with click reactions

---
+++ static/vendor/l2d-widget/autoload.js
@@ -0,0 +1,88 @@
+/*!
+ * Live2D Widget
+ * https://github.com/stevenjoezhang/live2d-widget
+ */
+
+// Recommended to use absolute path for live2d_path parameter
+// live2d_path 参数建议使用绝对路径
+const live2d_path = 'https://fastly.jsdelivr.net/npm/live2d-widgets@1.0.1/dist/';
+// const live2d_path = '/dist/';
+
+// Method to encapsulate asynchronous resource loading
+// 封装异步加载资源的方法
+function loadExternalResource(url, type) {
+  return new Promise((resolve, reject) => {
+    let tag;
+
+    if (type === 'css') {
+      tag = document.createElement('link');
+      tag.rel = 'stylesheet';
+      tag.href = url;
+    }
+    else if (type === 'js') {
+      tag = document.createElement('script');
+      tag.type = 'module';
+      tag.src = url;
+    }
+    if (tag) {
+      tag.onload = () => resolve(url);
+      tag.onerror = () => reject(url);
+      document.head.appendChild(tag);
+    }
+  });
+}
+
+(async () => {
+  // If you are concerned about display issues on mobile devices, you can use screen.width to determine whether to load
+  // 如果担心手机上显示效果不佳，可以根据屏幕宽度来判断是否加载
+  // if (screen.width < 768) return;
+
+  // Avoid cross-origin issues with image resources
+  // 避免图片资源跨域问题
+  const OriginalImage = window.Image;
+  window.Image = function(...args) {
+    const img = new OriginalImage(...args);
+    img.crossOrigin = "anonymous";
+    return img;
+  };
+  window.Image.prototype = OriginalImage.prototype;
+  // Load waifu.css and waifu-tips.js
+  // 加载 waifu.css 和 waifu-tips.js
+  await Promise.all([
+    loadExternalResource(live2d_path + 'waifu.css', 'css'),
+    loadExternalResource(live2d_path + 'waifu-tips.js', 'js')
+  ]);
+  // For detailed usage of configuration options, see README.en.md
+  // 配置选项的具体用法见 README.md
+  initWidget({
+    waifuPath: live2d_path + 'waifu-tips.json',
+    // cdnPath: 'https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/',
+    cubism2Path: live2d_path + 'live2d.min.js',
+    cubism5Path: 'https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js',
+    tools: ['hitokoto', 'asteroids', 'switch-model', 'switch-texture', 'photo', 'info', 'quit'],
+    logLevel: 'warn',
+    drag: false,
+  });
+})();
+
+console.log(`\n%cLive2D%cWidget%c\n`, 'padding: 8px; background: #cd3e45; font-weight: bold; font-size: large; color: white;', 'padding: 8px; background: #ff5450; font-size: large; color: #eee;', '');
+
+/*
+く__,.ヘヽ.        /  ,ー､ 〉
+         ＼ ', !-─‐-i  /  /´
+         ／｀ｰ'       L/／｀ヽ､
+       /   ／,   /|   ,   ,       ',
+     ｲ   / /-‐/  ｉ  L_ ﾊ ヽ!   i
+      ﾚ ﾍ 7ｲ｀ﾄ   ﾚ'ｧ-ﾄ､!ハ|   |
+        !,/7 '0'     ´0iソ|    |
+        |.从"    _     ,,,, / |./    |
+        ﾚ'| i＞.､,,__  _,.イ /   .i   |
+          ﾚ'| | / k_７_/ﾚ'ヽ,  ﾊ.  |
+            | |/i 〈|/   i  ,.ﾍ |  i  |
+           .|/ /  ｉ：    ﾍ!    ＼  |
+            kヽ>､ﾊ    _,.ﾍ､    /､!
+            !'〈//｀Ｔ´', ＼ ｀'7'ｰr'
+            ﾚ'ヽL__|___i,___,ンﾚ|ノ
+                ﾄ-,/  |___./
+                'ｰ'    !_,.:
+*/
