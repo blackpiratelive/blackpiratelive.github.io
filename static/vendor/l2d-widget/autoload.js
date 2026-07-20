f3dfcb71e6ae2ea005c7d888cc56bd250dd66586
blackpirateapps <github@blackpiratex.com>
2026-07-21 02:08:37 +0530

    translated live2d mascot tips and messages to english

--- static/vendor/l2d-widget/autoload.js
+++ static/vendor/l2d-widget/autoload.js
@@ -55,7 +55,7 @@ function loadExternalResource(url, type) {
   // For detailed usage of configuration options, see README.en.md
   // 配置选项的具体用法见 README.md
   initWidget({
-    waifuPath: live2d_path + 'waifu-tips.json',
+    waifuPath: '/vendor/l2d-widget/waifu-tips.json',
     // cdnPath: 'https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/',
     cubism2Path: live2d_path + 'live2d.min.js',
     cubism5Path: 'https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js',
